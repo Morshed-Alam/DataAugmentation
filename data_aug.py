ac34d5384b10f818dda58ea0e90e0e81307a6f4f
Morshed Alam <Morshed-Alam@users.noreply.github.com>
2020-11-19 23:10:51 +0600

    Update data_aug.py

--- data_aug.py
+++ data_aug.py
@@ -818,6 +818,70 @@ class RandomHSV(object):
         return img, bboxes, 'randomhsv_'
 
     
+class HSV(object):
+    """HSV Transform to vary hue saturation and brightness
+    
+    Hue has a range of 0-179
+    Saturation and Brightness have a range of 0-255. 
+    Chose the amount you want to change thhe above quantities accordingly. 
+    
+    
+    
+    
+    Parameters
+    ----------
+    hue : None or int
+        If None, the hue of the image is left unchanged.
+        
+    saturation : None or int
+        If None, the saturation of the image is left unchanged.
+        
+    brightness : None or int
+        If None, the brightness of the image is left unchanged.
+    
+    Returns
+    -------
+    
+    numpy.ndaaray
+        Transformed image in the numpy format of shape `HxWxC`
+    
+    numpy.ndarray
+        Resized bounding box co-ordinates of the format `n x 4` where n is 
+        number of bounding boxes and 4 represents `x1,y1,x2,y2` of the box
+        
+    """
+    
+    def __init__(self, hue = None, saturation = None, brightness = None):
+        if hue:
+            self.hue = hue 
+        else:
+            self.hue = 0
+            
+        if saturation:
+            self.saturation = saturation 
+        else:
+            self.saturation = 0
+            
+        if brightness:
+            self.brightness = brightness
+        else:
+            self.brightness = 0
+    
+    def __call__(self, img, bboxes):
+        
+        img = img.astype(int)
+        
+        a = np.array([hue, saturation, brightness]).astype(int)
+        img += np.reshape(a, (1,1,3))
+        
+        img = np.clip(img, 0, 255)
+        img[:,:,0] = np.clip(img[:,:,0],0, 179)
+        
+        img = img.astype(np.uint8)
+
+        return img, bboxes, 'hsv_'
+    
+    
 class Sequence(object):
 
     """Initialise Sequence object
